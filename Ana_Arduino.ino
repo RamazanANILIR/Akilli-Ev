34dd976f0228f33009b694529705b56ba14ca8af
Tuncer Ergin <ergin.tuncer@hotmail.com>
2017-10-30 13:43:55 +0300

    Smart home arduino code

---
+++ Ana_Arduino.ino
@@ -0,0 +1,84 @@
+#include <Stepper.h>
+#include <SoftwareSerial.h>
+SoftwareSerial digerArduino(10, 11);// RX, TX
+const int hiz = 64 * 32;
+Stepper perdeMotor(hiz, 5, 3, 2, 4);
+Stepper garajMotor(hiz, 9, 7, 6, 8);
+int zilButton = 12;
+char veri;
+char digerveri;
+boolean bebeketkin = false;
+int sicaklikdizisi[100];
+int i = 0;
+int toplam = 0;
+int neg=15;
+void setup() {
+  Serial.begin(9600);
+  digerArduino.begin(9600);
+  perdeMotor.setSpeed(10);
+  garajMotor.setSpeed(10);
+  pinMode(zilButton, INPUT);
+}
+
+void loop() {
+  sicaklikdizisi[i % 100] = (int)(analogRead(A0) * 0.48828125);
+  for (int j = 0; j < 100; j++) toplam += sicaklikdizisi[j];
+  Serial.print(toplam / 100-neg); //Sıcaklık ölçümündeki dalgalanmaları önlemek için son ölçülen 10 degeri bir diziye atıp ortalamasını aldım
+  Serial.print("#"); 
+  if(toplam/100-neg>30)neg++;
+  toplam = 0;
+  i++;
+ 
+  if (digitalRead(zilButton) == HIGH) {
+    digerArduino.print('e');
+    Serial.print("zil");//Androide Zil çaldı bildirimi için sinyal gönder
+    Serial.print("#");
+  } else if (digitalRead(zilButton) == LOW) {
+    digerArduino.print('f');
+  }
+  
+  if (analogRead(A1) > 800 && bebeketkin) {
+    Serial.print("bebek");
+    Serial.print("#");
+  }
+  
+  
+  if (digerArduino.available() > 0) {
+    digerveri = digerArduino.read();
+    if (digerveri == 'a') {
+      Serial.print("alarm");
+      Serial.print("#");
+    }
+  }
+  
+  
+  if (Serial.available() > 0) {
+    veri = Serial.read();
+    if (veri == 'a') {
+      for (int i = 0; i < 4 ; i++) {
+        perdeMotor.step(hiz);
+      }
+    } else if (veri == 'b') {
+      for (int i = 0; i < 5; i++) {
+        perdeMotor.step(-hiz);
+      }
+    } else if (veri == 'm') {
+      for (int i = 0; i < 4 ; i++) {
+        garajMotor.step(hiz);
+      }
+    } else if (veri == 'n') {
+      for (int i = 0; i < 4; i++) {
+        garajMotor.step(-hiz);
+      }
+    }
+    else if (veri == 'u') {
+      bebeketkin = true;
+    }
+    else if (veri == 'v') {
+      bebeketkin = false;
+    } else {
+      digerArduino.print(veri);
+    }
+  }
+  delay(100);
+}
---
+++ Ikinci_Arduino.ino
@@ -0,0 +1,87 @@
+int ketle = 2;
+int bahce = 3;
+int zil = 4 ;
+int lamba = 5 ;
+int fan = 6;
+int utu = 7;
+int camasirMakinasi = 8 ;
+int trigPin = 9;
+int echoPin = 10;
+int sacKurutmaMakinasi = 11 ;
+
+long sure;
+long uzaklik;
+char veri;
+boolean guvenliketkin = false;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ketle, OUTPUT);
+  pinMode(bahce, OUTPUT);
+  pinMode(zil, OUTPUT);
+  pinMode(lamba, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode(utu, OUTPUT);
+  pinMode(camasirMakinasi, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(sacKurutmaMakinasi, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW); /* sensör pasif hale getirildi */
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH); /* Sensore ses dalgasının üretmesi için emir verildi */
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);  /* Yeni dalgaların üretilmemesi için trig pini LOW konumuna getirildi */
+  sure = pulseIn(echoPin, HIGH); /* ses dalgasının geri dönmesi için geçen sure ölçülüyor */
+  uzaklik = sure / 29.1 / 2; /* ölçülen sure uzaklığa çevriliyor */
+  if (uzaklik < 4 && guvenliketkin) {
+    Serial.print("a");
+  }
+  
+  
+  
+  if (Serial.available() > 0) {
+    veri = Serial.read();
+    if (veri == 'k') {
+      digitalWrite(ketle, HIGH);
+    } else if (veri == 'l') {
+      digitalWrite(ketle, LOW);
+    } else if (veri == 'c') {
+      digitalWrite(bahce, HIGH);
+    } else if (veri == 'd') {
+      digitalWrite(bahce, LOW);
+    } else  if (veri == 'e') {
+      digitalWrite(zil, HIGH);
+    } else if (veri == 'f') {
+      digitalWrite(zil, LOW);
+    } else if (veri == 'g') {
+      digitalWrite(lamba, HIGH);
+    } else if (veri == 'h') {
+      digitalWrite(lamba, LOW);
+    } else if (veri == 'o') {
+      digitalWrite(fan, HIGH);
+    } else if (veri == 'p') {
+      digitalWrite(fan, LOW);
+    } else  if (veri == 'q') {
+      digitalWrite(utu, HIGH);
+    } else if (veri == 'r') {
+      digitalWrite(utu, LOW);
+    } else if (veri == 's') {
+      digitalWrite(camasirMakinasi, HIGH);
+    } else if (veri == 't') {
+      digitalWrite(camasirMakinasi, LOW);
+    } else if (veri == 'y') {
+      guvenliketkin = true;
+    } else if (veri == 'z') {
+      guvenliketkin = false;
+    } else if (veri == 'w') {
+      digitalWrite(sacKurutmaMakinasi, HIGH);
+    } else if (veri == 'i') {
+      digitalWrite(sacKurutmaMakinasi, LOW);
+    }
+  }
+  delay(100);
+}
